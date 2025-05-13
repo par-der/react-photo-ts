18da03597226c108c85b5f6c475cf7ed34bb91b3
par-der <egorterentev58@gmail.com>
2025-05-13 23:15:12 +0400

    first commit

--- src/components/search-input/search-input.tsx
+++ src/components/search-input/search-input.tsx
@@ -1,38 +1,31 @@
+import {Autocomplete, CircularProgress, TextField} from "@mui/material";
+
 interface SearchInputProps {
-    search: string
-    setSearch: (value: string) => void
+    search: string;
+    setSearch: (value: string) => void;
+    options: string[];
+    loading?: boolean;
 }
 
-const SearchInput = ({search, setSearch}: SearchInputProps) => {
+const SearchInput = ({search, setSearch, options, loading = false}: SearchInputProps) => {
     return (
-        <div className="relative w-full max-w-full">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                    className="w-5 h-5 text-gray-400"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                </svg>
-            </div>
-            <input
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                type="search"
-                placeholder="Поиск..."
-                className="w-full pl-10 pr-4 py-2.5 rounded-lg border border-gray-300
-                 focus:border-blue-500 focus:ring-2 focus:ring-blue-200
-                 transition-all duration-200 outline-none
-                 placeholder:text-gray-400 text-gray-700
-                 hover:border-gray-400"
+        <Autocomplete
+            options={options}
+            freeSolo
+            inputValue={search}
+            onInputChange={(_, newValue) => setSearch(newValue)}
+            renderInput={(params) => <TextField {...params} label="Поиск" InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                        <>
+                            {loading ? <CircularProgress size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                        </>
+                    ),
+                }}
             />
-        </div>
+            }
+        />
     );
 };
 
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,4 +1,4 @@
-import {useGetProductsQuery} from "../../services/api/queries.ts";
+import {useGetProductsQuery, useSearchProductsQuery} from "../../services/api/queries.ts";
 import Product from "../../components/product/product.tsx";
 import SearchInput from "../../components/search-input/search-input.tsx";
 import {useEffect, useState} from "react";
@@ -10,17 +10,23 @@ import {NAVIGATION_ROUTES} from "../../constants/routes.ts";
 
 const HomePage = () => {
     const [page, setPage] = useState(1);
+    //const [enabled, setEnabled] = useState(false);
     const [search, setSearch] = useState('');
-    const debouncedSearchTerm = useDebounce(search, 500);
+    const debouncedSearchTerm = useDebounce(search, 150);
+    const enabled = debouncedSearchTerm.trim().length > 0;
     const {data: products, isLoading, isError, error} = useGetProductsQuery({
         'page': String(page),
-        'search': debouncedSearchTerm
+        //'search': debouncedSearchTerm
     });
+    const {data: searchProducts, isLoading: isSearchLoading, isError: isSearchError, error: searchError} = useSearchProductsQuery(
+        debouncedSearchTerm,
+        enabled,
+    );
     const navigate = useNavigate();
-
-    useEffect(() => {
-        setPage(1);
-    }, [debouncedSearchTerm]);
+    const searchOptions: string[] = searchProducts?.results.map((product) => `${product.article} (${product.title})`) ?? [];
+    // useEffect(() => {
+    //     setPage(1);
+    // }, [debouncedSearchTerm]);
 
     if (isError) {
         return <div>Error: {error?.message}</div>;
@@ -30,7 +36,7 @@ const HomePage = () => {
         <div className="w-full flex flex-col gap-4 p-4 container mx-auto">
             <div className="w-full">
                 {/*TODO добавить debounced*/}
-                <SearchInput search={search} setSearch={setSearch}/>
+                <SearchInput search={search} setSearch={setSearch} options={searchOptions} loading={isSearchLoading} />
             </div>
             <div className="w-full flex items-center border border-gray-200 gap-1.5 p-4 rounded-lg bg-neutral-50 ">
                 <Button onClick={() => navigate(NAVIGATION_ROUTES.PRODUCTS_ADD)} variant="contained"
